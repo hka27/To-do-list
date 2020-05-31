e57181e501219fb5a860479b5e4acf90597603c4
hka27 <harry.hemant2@gmail.com>
2020-05-31 21:49:08 +0530

    states are saved now & beautiful UI is also added.

--- app.js
+++ app.js
@@ -1,20 +1,39 @@
-//SLECTORS
+//SLECTORS..........................................
 const taskInput = document.querySelector('.task-input');
 const submitBtn = document.querySelector('.submit-btn');
 const itemListDiv = document.querySelector('.item-list-div');
 
-if (localStorage) {
-  console.log(localStorage);
-  Object.keys(localStorage).forEach((item) => {
-    // console.log(item);
-    console.log(item, localStorage.getItem(item));
+//ADDING DYNAMIC BACKGROUND.........................
+setInterval(() => {
+  const date =
+    new Date().getHours().toString() +
+    new Date().getMinutes() +
+    new Date().getSeconds();
+  document.body.style.backgroundColor = `#${date}`;
+}, 1000);
+
+//RETAINING STATES FROM LOCALSTORAGE................
+// console.log(localStorage);
+const localData = localStorage.getItem('data');
+const parsedData = JSON.parse(localData);
+// console.log(parsedData);
+
+//initialising global data for storing the data and thier states
+const data = [];
+
+//PUSHING RETRIEVED STATES TO THE UI...............
+if (parsedData) {
+  parsedData.forEach((item) => {
+    data.push(item);
+
     const itemDiv = document.createElement('div');
     itemDiv.classList.add('item-div');
+
     const itemTextDiv = document.createElement('div');
     itemTextDiv.classList.add('item-text-div');
 
     const itemText = document.createElement('p');
-    itemText.textContent = item;
+    itemText.textContent = item[0];
     itemTextDiv.append(itemText);
 
     const itemBtnDiv = document.createElement('div');
@@ -24,38 +43,58 @@ if (localStorage) {
     checkBtn.textContent = 'check';
     const delBtn = document.createElement('button');
     delBtn.textContent = 'delete';
+
     itemBtnDiv.append(checkBtn, delBtn);
     itemDiv.append(itemTextDiv, itemBtnDiv);
-    if (localStorage.getItem(item) == 'true') {
+
+    if (item[1] == true) {
       itemDiv.classList.add('task-completed');
-      // console.log(item);
     }
     itemListDiv.append(itemDiv);
   });
 }
 
-//EVENT_LISTENERS
+//EVENT LISTENERS.................................
 submitBtn.addEventListener('click', addItem);
 itemListDiv.addEventListener('click', taskIt);
-//FUNCTIONS
+
+//FUNCTIONS......................................
 function taskIt(event) {
+  event.preventDefault();
   // console.log(event.target.innerText);
   const el = event.target.parentNode.parentNode.children[0].innerText;
+
   if (event.target.innerText == 'check') {
     event.target.parentNode.parentNode.classList.toggle('task-completed');
 
-    localStorage.removeItem(el);
-    localStorage.setItem(el, true);
+    data.forEach((curr, index) => {
+      // console.log(curr[1]);
+      if (curr[0] === el) {
+        if (data[index][1] == true) {
+          data[index][1] = false;
+        } else {
+          data[index][1] = true;
+        }
+        return;
+      }
+    });
   }
   if (event.target.innerText == 'delete') {
-    // console.log(event.target.parentNode.parentNode.remove());
-
     event.target.parentNode.parentNode.remove();
     // console.log(el);
-    localStorage.removeItem(el);
+    data.forEach((curr, index) => {
+      if (curr[0] === el) {
+        data.splice(index, 1);
+        return;
+      }
+    });
   }
 }
-const data = [];
+
+// data was copied in the beginning from local-storage to global variable data[].
+// console.log(data);
+
+//ADD ITEMS TO UI AND DATA[].....................
 function addItem(event) {
   event.preventDefault();
   const itemDiv = document.createElement('div');
@@ -80,14 +119,19 @@ function addItem(event) {
     itemDiv.append(itemTextDiv, itemBtnDiv);
     itemListDiv.append(itemDiv);
 
-    data.push(taskInput.value);
-    localStorage.setItem(taskInput.value, false);
+    //updating data to data[];
+    data.push([taskInput.value, false]);
+    // console.log(data);
 
+    // clearing the input fields.
     taskInput.value = '';
     taskInput.focus();
   } else {
     alert('Please Enter Something !!! ');
   }
-
-  //   console.log(data);
 }
+
+//SETTING LOCAL_STORAGE TO DATA[] WHEN WINDOW IS ABOUT TO CLOSED................
+window.addEventListener('beforeunload', function (e) {
+  localStorage.setItem('data', JSON.stringify(data));
+});
